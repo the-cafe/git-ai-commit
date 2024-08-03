eae3afca56128cfe3ec1ad3e3e60cc500b3c36b9
Ming <minglianglin50@gmail.com>
2024-08-03 18:43:04 -0400

    add gitignore

--- prepare_commit_msg_hooks/openai_service.py
+++ prepare_commit_msg_hooks/openai_service.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 class OpenAiService:
     def __init__(self):
-      api_key = os.environ.get("OPENAI_API_KEY")
+      api_key = "sk-proj-pPkf0GH5LzbVFno8Eyc1T3BlbkFJY628EJOBgGN6wNMN494j"
 
       if(api_key is None):
         raise Exception("OPENAI_API_KEY is not set")
-      self.client = OpenAI()
+      self.client = OpenAI(api_key="sk-proj-pPkf0GH5LzbVFno8Eyc1T3BlbkFJY628EJOBgGN6wNMN494j")
 
     def chat_with_openai(self, messages):
         completion = self.client.chat.completions.create(
