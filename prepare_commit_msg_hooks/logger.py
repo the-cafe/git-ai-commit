ba7d80e46927469fb11409544a4140a3a8722dae
Ming <minglianglin50@gmail.com>
2024-08-04 22:57:53 -0400

    ✨add logger module for commit message logging ✨add logger class for commit message logging

---
+++ prepare_commit_msg_hooks/logger.py
@@ -0,0 +1,32 @@
+import os
+from prepare_commit_msg_hooks.utils import get_git_directory
+from datetime import datetime, timedelta, timezone
+
+def get_current_time():
+  EST = timezone(timedelta(hours=-5))  # Adjusted to UTC-5 for Eastern Standard Time
+  now_utc = datetime.now(timezone.utc)  # Get the current time in UTC
+  now_est = now_utc.astimezone(EST)  # Convert UTC to Eastern Standard Time
+
+  # Format the datetime object to exclude milliseconds
+  current_time = now_est.strftime('%Y-%m-%dT%H:%M:%S%z')  # ISO 8601 format without milliseconds
+
+  return str(current_time)
+
+class Logger:
+  def __init__(self):
+    cwd =  get_git_directory()
+    self.log_file = cwd + "/ai_commit_message.log"
+
+  def log(self, message):
+    print(message)
+    new_line = "" if message.endswith('\n') else "\n"
+
+    log_line = self.getPrefix() + message + new_line
+
+    with open(self.log_file, 'a') as file:
+      file.write(log_line)
+
+  def getPrefix(self):
+    logged_string = "[" + get_current_time() + "] "
+
+    return logged_string
