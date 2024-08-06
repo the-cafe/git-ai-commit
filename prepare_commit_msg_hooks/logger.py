80644e36e9baf8af17307bf2fd81dff53c8dabea
Ming <minglianglin50@gmail.com>
2024-08-06 01:31:02 -0400

    ✨add config service for logger functionality ✨add config service for logger enabled check

--- prepare_commit_msg_hooks/logger.py
+++ prepare_commit_msg_hooks/logger.py
@@ -1,6 +1,6 @@
-import os
 from prepare_commit_msg_hooks.utils import get_git_directory
 from datetime import datetime, timedelta, timezone
+from prepare_commit_msg_hooks.config_service import ConfigServiceSingleton
 
 def get_current_time():
   EST = timezone(timedelta(hours=-5))  # Adjusted to UTC-5 for Eastern Standard Time
@@ -14,19 +14,23 @@ def get_current_time():
 
 class Logger:
   def __init__(self):
-    cwd = get_git_directory()
-    self.log_file = cwd + "/ai_commit_message.log"
+    repo_git_directory = get_git_directory()
+    self.log_file = repo_git_directory + "/ai_commit_message.log"
 
   def log(self, message):
-    print(message)
+    # Check if the logger is enabled
+    if not self.is_enabled():
+      return
+
     new_line = "" if message.endswith('\n') else "\n"
-
-    log_line = self.getPrefix() + message + new_line
-
+    log_line = self.get_prefix() + message + new_line
     with open(self.log_file, 'a') as file:
       file.write(log_line)
 
-  def getPrefix(self):
+  def get_prefix(self):
     logged_string = "[" + get_current_time() + "] "
 
     return logged_string
+
+  def is_enabled(self):
+    return ConfigServiceSingleton.logger_enabled
