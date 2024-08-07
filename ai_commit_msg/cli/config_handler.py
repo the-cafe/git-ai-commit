55ae2acbc6e78f3b4bc465432ab5d20a0f964c47
Ming <minglianglin50@gmail.com>
2024-08-07 15:39:22 -0400

    ✨remove cli.py file, rename cli_handler to cli

--- ai_commit_msg/cli/config_handler.py
+++ ai_commit_msg/cli/config_handler.py
@@ -1,9 +1,11 @@
 
-
 from ai_commit_msg.services.openai_service import OpenAiService
 from ai_commit_msg.utils.logger import Logger
 
 def config_handler(args):
+    if(args.openai_key is None):
+        Logger().log("OpenAI API key is required")
+        return
     OpenAiService.set_openai_api_key(args.openai_key)
 
     Logger().log("OpenAI API key set successfully")
--- ai_commit_msg/main.py
+++ ai_commit_msg/main.py
@@ -1,9 +1,24 @@
 from __future__ import annotations
 
+import argparse
+from typing import Sequence
+from ai_commit_msg.cli.config_handler import config_handler
 from ai_commit_msg.prepare_commit_msg_hook import prepare_commit_msg_hook
 
-def main() -> int:
-    prepare_commit_msg_hook()
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser(description="CLI tool")
+    subparsers = parser.add_subparsers(dest='command', required=False)
+
+    # Config command
+    config_parser = subparsers.add_parser('config', help='Configure the tool')
+    config_parser.add_argument('--openai-key', required=True, help='Open AI API key, this will only be stored locally')
+
+    args = parser.parse_args(argv)
+
+    if args.command == 'config':
+        config_handler(args)
+    else:
+        prepare_commit_msg_hook()
 
     return 0
 
