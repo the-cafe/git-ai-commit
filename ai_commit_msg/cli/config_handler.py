5ece90efbf77f1aeb403fd8323a5d4855bc56b41
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-08-09 00:02:39 -0400

    ✨simplify openai_key and logger checks in config_handler

--- ai_commit_msg/cli/config_handler.py
+++ ai_commit_msg/cli/config_handler.py
@@ -3,7 +3,7 @@ from ai_commit_msg.services.config_service import ConfigService
 from ai_commit_msg.utils.logger import Logger
 
 def config_handler(args):
-    if args.openai_key is not None:
+    if args.openai_key:
         if args.openai_key.strip() == "":
             raise ValueError("OpenAI API key is not set. Run the following command to set the key: gen_ai_commit_message_cli config --openai-key=<insert-your-key>")
         OpenAiService.set_openai_api_key(args.openai_key)
@@ -11,7 +11,7 @@ def config_handler(args):
     elif args.reset:
         OpenAiService.reset_openai_api_key()
         Logger().log("OpenAI API key has been reset")
-    elif args.logger is not None:
+    elif args.logger:
         ConfigService.set_logger_enabled(args.logger)
     else:
         Logger().log("No valid configuration option provided")
