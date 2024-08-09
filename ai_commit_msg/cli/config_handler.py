66dce5310399bf3219a70d388bd55c1e7ddafb5d
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-08-09 00:22:27 -0400

    ✨add print statement for debugging purpose ✨added staged files check before committing changes

--- ai_commit_msg/cli/config_handler.py
+++ ai_commit_msg/cli/config_handler.py
@@ -7,6 +7,7 @@ def config_handler(args):
         OpenAiService.set_openai_api_key(args.openai_key)
         Logger().log("OpenAI API key set successfully")
         print("OpenAI API key set successfully")
+        print("printing")
         return
 
     return 0
