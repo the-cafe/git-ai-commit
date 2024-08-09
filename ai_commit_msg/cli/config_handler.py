afd6af34262184e21c1b4c12fc293257f159c1ce
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-08-08 20:01:04 -0400

    ✨update error handling for empty openai key

--- ai_commit_msg/cli/config_handler.py
+++ ai_commit_msg/cli/config_handler.py
@@ -5,7 +5,8 @@ from ai_commit_msg.utils.logger import Logger
 def config_handler(args):
     if args.openai_key is not None:
         if args.openai_key.strip() == "":
-            raise ValueError("OpenAI API key is not set. Run the following command to set the key: gen_ai_commit_message_cli config --openai-key=<insert-your-key>")
+            print("OpenAI API key is not set. Run the following command to set the key: gen_ai_commit_message config --openai-key=<insert-your-key>")
+            return;
         OpenAiService.set_openai_api_key(args.openai_key)
         Logger().log("OpenAI API key set successfully")
     elif args.reset:
