bfa80106cf4b0d8c0abcd4e817db0500c0323c12
Ming <minglianglin50@gmail.com>
2024-08-10 03:51:50 -0400

    ✨Add a period at the end of the sentence.

--- ai_commit_msg/core/prompt.py
+++ ai_commit_msg/core/prompt.py
@@ -2,7 +2,7 @@ COMMIT_MSG_SYSTEM_MESSAGE = '''
 You will be provided with a set of code changes in diff format.
 Your task is to read each file and explain every major change in a concise way.
 
-Be sure to include details of major changes
+Be sure to include details of major changes.
 
 You don't need to add any punctuation or capitalization.
 
