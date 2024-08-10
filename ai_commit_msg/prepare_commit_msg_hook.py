4a6bde49abed12c41b3efe9775c36af6c4d59cc3
Ming <minglianglin50@gmail.com>
2024-08-10 01:02:01 -0400

    manually typed commit msg

--- ai_commit_msg/prepare_commit_msg_hook.py
+++ ai_commit_msg/prepare_commit_msg_hook.py
@@ -4,8 +4,11 @@ from ai_commit_msg.utils.logger import Logger
 
 def prepare_commit_msg_hook():
   existing_content = GitService.read_commit_editmsg_file()
-
   Logger().log("Existing content: " + existing_content)
+
+  filtered_content = "\n".join([line for line in existing_content.splitlines() if not line.strip().startswith('#')])
+
+  Logger().log("Filtered content: " + filtered_content)
 
   if(existing_content != ""):
     Logger().log("Commit message already exists, skipping AI commit message generation")
