d6903a4393a4e824bac35e1829f83f0dcd8549b3
Ming <minglianglin50@gmail.com>
2024-08-10 02:38:31 -0400

    ✨add has_updated flag to track config changes ✨added has_updated flag for tracking config changes enable support for setting multiple config attributes

--- ai_commit_msg/cli/config_handler.py
+++ ai_commit_msg/cli/config_handler.py
@@ -6,28 +6,32 @@ from ai_commit_msg.utils.logger import Logger
 def config_handler(args):
     config_service = ConfigService()
 
+    has_updated = False
+
     if args.openai_key:
         OpenAiService.set_openai_api_key(args.openai_key)
         Logger().log("OpenAI API key set successfully")
-        return None
+        has_updated = True
 
     if args.reset:
         # reset the db the entire db
         LocalDbService().reset_db();
         Logger().log("OpenAI API key has been reset")
-        return None
+        has_updated = True
 
     if args.logger is not None:
         config_service.set_logger_enabled(args.logger)
         Logger().log("Logger " + ("enabled" if args.logger else "disabled"))
-        return None
+        has_updated = True
 
     if args.model:
         config_service.set_model(args.model)
         Logger().log("Model set to " + args.model)
-        return None
+        has_updated = True
 
-    display_config_db = LocalDbService().display_db()
-    Logger().log(display_config_db)
+    #if there are not args, display the current config
+    if not has_updated:
+        display_config_db = LocalDbService().display_db()
+        Logger().log(display_config_db)
 
     return None
