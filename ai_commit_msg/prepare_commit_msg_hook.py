4a4b1ff780994647e99a96094278c03c908abf30
Ming <minglianglin50@gmail.com>
2024-08-10 01:06:38 -0400

    ✨Remove lines that start with "#", filter out existing content, generate an AI-powered commit message and update the commit message.

--- ai_commit_msg/prepare_commit_msg_hook.py
+++ ai_commit_msg/prepare_commit_msg_hook.py
@@ -4,19 +4,15 @@ from ai_commit_msg.utils.logger import Logger
 
 def prepare_commit_msg_hook():
   existing_content = GitService.read_commit_editmsg_file()
-  Logger().log("Existing content: " + existing_content)
 
+  # filter out any lines that start with a "#"
   filtered_content = "\n".join([line for line in existing_content.splitlines() if not line.strip().startswith('#')])
-
-  Logger().log("Filtered content: " + filtered_content.strip())
 
   if(filtered_content != ""):
     Logger().log("Commit message already exists, skipping AI commit message generation")
     return
 
   commit_message = "✨" + generate_commit_message()
-
-  Logger().log("Generated commit message: " + commit_message)
 
   GitService.update_commit_message(commit_message)
 
