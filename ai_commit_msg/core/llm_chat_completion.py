274acd7e7fce7b570796384479d3224734141d94
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2025-03-20 18:41:32 -0400

    refactor llm_chat_completion function to improve clarity

--- ai_commit_msg/core/llm_chat_completion.py
+++ ai_commit_msg/core/llm_chat_completion.py
@@ -6,12 +6,10 @@ from ai_commit_msg.utils.logger import Logger
 def llm_chat_completion(prompt):
     select_model = ConfigService.get_model()
 
-    # Use the factory to create the appropriate service
     service = LLMServiceFactory.create_service(select_model)
 
     if service is None:
         Logger().log("Unsupported model: " + select_model)
         return ""
 
-    # Use the unified interface to call the service
     return service.chat_completion(prompt)
